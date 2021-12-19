b57b32836c2fd5098ac5e9d871c858fcd2730f3a
Oleg Agapov <oleg.agapov.ua@gmail.com>
2021-12-19 14:46:49 +0100

    Fix incorrect link

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -2,7 +2,7 @@
     <div class="prose prose-green p-8">
         <p>
             I work for
-            <a href="https://gog.com" target="_blank">Palta</a> as BI Engineer.
+            <a href="https://palta.com" target="_blank">Palta</a> as BI Engineer.
         </p>
         <p>My <router-link to="/cv">work experience</router-link> (CV).</p>
         <p>
