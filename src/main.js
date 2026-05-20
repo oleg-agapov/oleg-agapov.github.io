1a5e9872bc2ad4dbd3e1c083d88dc1673e4c3c05
Oleg Agapov <oleg.agapov.ua@gmail.com>
2026-05-19 20:26:23 -0700

    Clean-up

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './index.css'
-
-createApp(App).use(router).use(store).mount('#app')
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex'
-import user from '/src/store/modules/user'
-import variables from '/src/store/modules/variables'
-
-const store = createStore({
-    modules: { user, variables },
-})
-
-export default store
